1865338ad8c29ae38544e62837db0a46ee227cf7
Anna <you@example.com>
2021-09-10 10:51:44 +0100

    Config files now include Boto3 CONSTANTS

--- FinalProject/config_manager.py
+++ FinalProject/config_manager.py
@@ -1,9 +1,22 @@
 from definitions import PROJECT_ROOT_DIR
 import configparser
 import os
+import boto3
 
 _config = configparser.ConfigParser()
 
 _config.read(os.path.join(PROJECT_ROOT_DIR, 'config.ini'))
 
-S3_URL = _config['default']['url']
+S3_BUCKET = _config['default']['url']
+S3_CLIENT = boto3.client('s3')
+S3_RESOURCE = boto3.resource('s3')
+
+BUCKET_CONTENTS_TALENT = S3_CLIENT.list_objects_v2(
+        Bucket=S3_BUCKET,
+        Prefix='Talent'
+    )
+
+BUCKET_CONTENTS_ACADEMY = S3_CLIENT.list_objects_v2(
+        Bucket=S3_BUCKET,
+        Prefix='Academy'
+    )
