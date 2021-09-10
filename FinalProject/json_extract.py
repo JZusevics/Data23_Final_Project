3ba2233af64f05063dcfb1e0f476be42147057b9
Conjr123 <Conjr123>
2021-09-10 12:09:41 +0100

    Pushing up edited json extract with comments

--- FinalProject/json_extract.py
+++ FinalProject/json_extract.py
@@ -10,6 +10,12 @@ s3_client = boto3.client('s3')
 bucket_name = 'data23-finalproject-2'
 
 def extract_json(key, dictionaries):
+    f"""
+    
+    :param key: Selects s3 key to extract
+    :param dictionaries: Initially empty dictionary to have json files appended to
+    :return: dictionary of json names and files {"name_and_date": "json"}
+    """
     s3_object = s3_client.get_object(
         Bucket=bucket_name,
         Key=key
@@ -22,6 +28,12 @@ def extract_json(key, dictionaries):
     return dictionaries
 
 def extract_just_json(bucket_name):
+    """
+    This file is the final function to allow for the exctraction of just json files from the s3 bucket
+    it will not be run in main
+    :param bucket_name: The location of the s3 bucket
+    :return: A dictionary of json files and names for conversion into a json file
+    """
     paginator = s3_client.get_paginator('list_objects_v2')
     pages = paginator.paginate(Bucket=bucket_name, Prefix='Talent')
     empty_dicts = {}
@@ -32,5 +44,6 @@ def extract_just_json(bucket_name):
         return extracted_dicts
 
 x = extract_just_json(bucket_name)
+# saves json onto local machine
 with open('data.json', 'w') as fp:
     json.dump(x, fp, sort_keys=True, indent=4)
