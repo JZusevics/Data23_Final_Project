524d021bc86ad2c9bd4ae5315c79ae7badd82f0a
Conjr123 <Conjr123>
2021-09-13 10:46:34 +0100

    Pushing up edited json cleaning file for Anna to have a look

--- FinalProject/datacleaningjson.py
+++ FinalProject/datacleaningjson.py
@@ -29,7 +29,9 @@ def date_test(name_dates, format, candidate_json):
 
 yes_no_keys = ['self_development', 'financial_support_self', 'geo_flex']
 # test that bools are of the right values
-def
+
+
+def bool_fix(name_dates, candidate_json):
     for candidate in name_dates:
         for key in yes_no_keys:
             test = str(candidate_json[candidate][key].strip())
@@ -40,27 +42,32 @@ def
             else:
                 # returns incorrect data for error exploration
                 print(test)
-count = 0
-for candidate in name_dates:
-    test = str(candidate_json[candidate]['result'])
-    candidate_json[candidate]['pass'] = candidate_json[candidate]['result']
-    del candidate_json[candidate]['result']
-    if test == 'Pass':
-        candidate_json[candidate]['pass'] = True
-        count += 1
-    elif test == 'Fail':
-        candidate_json[candidate]['pass'] = False
-    else:
-        # returns incorrect data for error exploration
-        print(test)
-    print(count)
+    return candidate_json
+
+
+def results_fix(name_date, candidate_json):
+    for candidate in name_dates:
+        test = str(candidate_json[candidate]['result'])
+        candidate_json[candidate]['pass'] = candidate_json[candidate]['result']
+        del candidate_json[candidate]['result']
+        if test == 'Pass':
+            candidate_json[candidate]['pass'] = True
+            count += 1
+        elif test == 'Fail':
+            candidate_json[candidate]['pass'] = False
+        else:
+            # returns incorrect data for error exploration
+            print(test)
+    return candidate_json
+
+
 def name_strip(name_dates ,candidate_json):
     for candidate in name_dates:
         candidate_json[candidate]['name'] = str(candidate_json[candidate]['name']).strip().lower().title()
         if str(candidate_json[candidate]['name']).strip().lower().title().count(' ') > 1:
             print(str(candidate_json[candidate]['name']).strip().lower().title())
-
     return name_dates
+
 
 def skill_id_generator(name_dates, skill):
     list_strengths = []
@@ -107,16 +114,13 @@ def tech_skill_id_generator(name_dates, skill):
            pass
     return strengths_id
 
-
-def candidate_id_generator(name_dates, skill):
-    i = 1
-
-
 name_strip(name_dates, candidate_json)
 candidate_json = date_test(name_dates, date_format, candidate_json)
 strengths_id = skill_id_generator(name_dates, 'strengths')
 weakness_id = skill_id_generator(name_dates, 'weaknesses')
 tech_skill_id = tech_skill_id_generator(name_dates, 'tech_self_score')
+candidate_json = bool_fix(name_dates, candidate_json)
+candidate_json = results_fix(name_date, candidate_json)
 
 with open('data_clean.json', 'w') as fp:
     json.dump(candidate_json, fp, sort_keys=True, indent=4)
