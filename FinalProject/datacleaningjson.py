8d6a46e18949afc7fba9254d82bef24534595d6c
Conjr123 <Conjr123>
2021-09-14 10:07:13 +0100

    Pushing up most recent version of json cleaning for testing

--- FinalProject/datacleaningjson.py
+++ FinalProject/datacleaningjson.py
@@ -1,37 +1,57 @@
 import json
 import datetime
+import numpy
+import pandas as pd
+import os
+import csv
+
+
+fname = os.path.join("connor.csv")
+generated_id = pd.read_csv(fname)
+
+desired_width=320
+pd.set_option('display.width', desired_width)
+numpy .set_printoptions(linewidth=desired_width)
+pd.set_option('display.max_columns',10)
+
 f = open('data.json')
 candidate_json = json.load(f)
+candidate_id = 0
 
-#print(candidate_json)
 
-
-name_dates = candidate_json.keys()
-date_format = "%d-%m-%Y"
+def internal_id_append(name_dates, candidate_json, candidate_id):
+    for name in list(name_dates):
+        candidate_json[name]['internal_id'] = candidate_id
+        candidate_id += 1
+    return candidate_json
 
 
 def date_test(name_dates, format, candidate_json):
     for candidate in name_dates:
-         date = candidate_json[candidate]['date']
-         date = date.replace('/', '-')
-         try:
-             datetime.datetime.strptime(date, format)
-             candidate_json[candidate]['date'] = date
-         except ValueError:
-             # fixes standard error of too many dases
+        date = candidate_json[candidate]['date']
+        date = date.replace('/', '-')
+        try:
+            date = datetime.datetime.strptime(date, format).strftime("%y/%m/%d")
+            candidate_json[candidate]['date'] = date
+        except ValueError:
+            # fixes standard error of too many dashes
              date = date.replace('-', '', 1)
              try:
-                 datetime.datetime.strptime(date, format)
+                 date = datetime.datetime.strptime(date, format).strftime("%y/%m/%d")
                  candidate_json[candidate]['date'] = date
              except ValueError:
                 print("Value error date ", date, " not in correct format")
     return candidate_json
 
-yes_no_keys = ['self_development', 'financial_support_self', 'geo_flex']
-# test that bools are of the right values
 
-
-def bool_fix(name_dates, candidate_json):
+def bool_fix(name_dates, candidate_json, yes_no_key):
+    """
+    Test that bool the three boolean key date pairs of financial_support_self, geo_flex and self_development are true
+    :param name_dates: The dictionary key we are using
+    :param candidate_json: Original json file
+    :param yes_no_key: List of three keys we are testing
+    :return:
+    """
     for candidate in name_dates:
         for key in yes_no_keys:
             test = str(candidate_json[candidate][key].strip())
@@ -45,7 +65,7 @@ def bool_fix(name_dates, candidate_json):
     return candidate_json
 
 
-def results_fix(name_date, candidate_json):
+def results_fix(name_dates, candidate_json):
     for candidate in name_dates:
         test = str(candidate_json[candidate]['result'])
         candidate_json[candidate]['pass'] = candidate_json[candidate]['result']
@@ -64,9 +84,46 @@ def name_strip(name_dates ,candidate_json):
     for candidate in name_dates:
         candidate_json[candidate]['name'] = str(candidate_json[candidate]['name']).strip().lower().title()
         # code commented out can be used to check for incorrect name formats
-        #if str(candidate_json[candidate]['name']).strip().lower().title().count(' ') > 1:
-         #   print(str(candidate_json[candidate]['name']).strip().lower().title())
-    return name_dates
+        # if str(candidate_json[candidate]['name']).strip().lower().title().count(' ') > 1:
+        # print(str(candidate_json[candidate]['name']).strip().lower().title())
+    return candidate_json
+
+
+def split_names(df: pd.DataFrame):
+    last_name_prefix = ["Van", "Le", "Di", "O", "De", "Du", "Von", "St", "Mc", "Mac", "La"]
+    first_name = []
+    middle_name = []
+    last_name = []
+
+    for name in df["name"]:
+        # Split names on spaces
+        split_name = name.split()
+        # If more than 2 names
+        if len(split_name) > 2:
+            # If the first name and second name are the same put them together
+            if split_name[0] == split_name[1]:
+                split_name = [" ".join(split_name[0:2])] + split_name[2:]
+            # If name contains a prefix put the following names together
+            for sub_name in last_name_prefix:
+                if sub_name in split_name:
+                    # Check if prefix is in the first name only put first and second together
+                    if split_name.index(sub_name) == 0:
+                        split_name = ["".join(split_name[split_name.index(sub_name):2])]+ split_name[2::]
+                    if sub_name in split_name:
+                        split_name = split_name[0:split_name.index(sub_name)]+[" ".join(split_name[split_name.index(sub_name)::])]
+
+        first_name.append(split_name[0])
+        last_name.append(split_name[-1])
+        if len(split_name) == 2:
+            middle_name.append(numpy.nan)
+        else:
+            middle_name.append("".join(split_name[1:-1]))
+
+    df.insert(1, "First Name", first_name, True)
+    df.insert(2, "Middle Name", middle_name, True)
+    df.insert(3, "Last Name", last_name, True)
+    df.drop(columns="name", inplace=True)
+    return df
 
 
 def skill_id_generator(name_dates, skill):
@@ -80,12 +137,13 @@ def skill_id_generator(name_dates, skill):
     strengths_id = {}
     for strength in list_strengths:
         strengths_id.update({strength: i})
-        i = i+1
+        i += 1
     for candidate in name_dates:
         strengths = candidate_json[candidate][skill]
         for i, strength in enumerate(strengths):
             id = strengths_id[strength]
             candidate_json[candidate][skill][i] = id
+    strengths_id = {value: key for key, value in strengths_id.items()}
     return strengths_id
 
 
@@ -112,15 +170,66 @@ def tech_skill_id_generator(name_dates, skill):
                 del candidate_json[candidate][skill][strength]
         except KeyError:
            pass
+    strengths_id = {value: key for key, value in strengths_id.items()}
     return strengths_id
 
-name_strip(name_dates, candidate_json)
-candidate_json = date_test(name_dates, date_format, candidate_json)
-strengths_id = skill_id_generator(name_dates, 'strengths')
-weakness_id = skill_id_generator(name_dates, 'weaknesses')
-tech_skill_id = tech_skill_id_generator(name_dates, 'tech_self_score')
-candidate_json = bool_fix(name_dates, candidate_json)
-candidate_json = results_fix(name_dates, candidate_json)
 
+def table_creator(dict, table_name, index_name):
+    dataframe = pd.Series(dict, name=table_name)
+    dataframe.index.name = index_name
+    dataframe.reset_index()
+    return dataframe
+
+
+def strengths_candidate_id_creator(json_keys, json, key):
+    df_list = []
+    for name in json_keys:
+        try:
+            for item in json[name][key]:
+                df_list.append([json[name]['internal_id'], item])
+        except KeyError:
+            pass
+    df = pd.DataFrame(df_list, columns=['id','skill'])
+    return df
+
+
+yes_no_keys = ['self_development', 'financial_support_self', 'geo_flex']
+candidate_json_keys = candidate_json.keys()
+date_format = "%d-%m-%Y"
+
+
+internal_id_append(candidate_json_keys, candidate_json, candidate_id)
+candidate_json = name_strip(candidate_json_keys, candidate_json)
+candidate_json = date_test(candidate_json_keys, date_format, candidate_json)
+candidate_json = bool_fix(candidate_json_keys, candidate_json, yes_no_keys)
+candidate_json = results_fix(candidate_json_keys, candidate_json)
+strengths_id = skill_id_generator(candidate_json_keys, 'strengths')
+weakness_id = skill_id_generator(candidate_json_keys, 'weaknesses')
+tech_skill_id = tech_skill_id_generator(candidate_json_keys, 'tech_self_score')
+strength_ids_df = table_creator(strengths_id, 'strength_ids', 'Strength_id')
+weakness_ids_df = table_creator(weakness_id, 'weakness_ids_df', 'weakness_id')
+tech_skill_ids_df = table_creator(tech_skill_id, 'tech_skill_ids_df', 'tech_skill_id')
+strength_table= strengths_candidate_id_creator(candidate_json_keys, candidate_json, 'strengths')
+weaknesses_table = strengths_candidate_id_creator(candidate_json_keys, candidate_json,'tech_self_score')
+tech_skill_table = strengths_candidate_id_creator(candidate_json_keys, candidate_json,'weaknesses')
+
+
+list_obj = []
+for key in list(candidate_json_keys):
+    #print(candidate_json[name])
+    del candidate_json[key]['strengths']
+    del candidate_json[key]['weaknesses']
+    try:
+        del candidate_json[key]['tech_self_score']
+    except KeyError:
+        pass
+    list_obj.append(candidate_json[key])
+candidate_df = pd.DataFrame(list_obj)
+
+df = split_names(candidate_df)
+
+print(df)
 with open('data_clean.json', 'w') as fp:
     json.dump(candidate_json, fp, sort_keys=True, indent=4)
+
+df.to_csv('df.csv', index=False)
