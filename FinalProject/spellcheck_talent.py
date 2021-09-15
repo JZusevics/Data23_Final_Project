59057e38fc393f7cdffa7a9c8ac9eab85729f424
Vrinda Gera <88028643+CodingUnicorn15@users.noreply.github.com>
2021-09-15 14:40:54 +0100

    Update spellcheck_talent.py

--- FinalProject/spellcheck_talent.py
+++ FinalProject/spellcheck_talent.py
@@ -1,14 +1,11 @@
 import pandas as pd
 
-def invited_spelling_talent(df: pd.DataFrame):
+def spelling_clean_talent(df: pd.DataFrame):
     """
-    
+
     :param df: csv dataframe generated from the extraction stage of the ETL pipeline
     :return: dataframe with correct spelling
     """
-    for item in df.invited_by_name:
-        if item == "Fifi Eton":
-            item = "Fifi Etton"
-        if item == "Belbrook":
-            item = "Bellbrook"
+    df.invited_by_name.replace("Fifi Eton", "Fifi Etton", inplace=True)
+    df.invited_by_name.replace("Belbrook", "Bellbrook", inplace=True)
     return df
