a23039f0949410ea54926a84f617455e04070190
Vrinda Gera <88028643+CodingUnicorn15@users.noreply.github.com>
2021-09-15 14:13:27 +0100

    Create spellcheck_talent.py

---
+++ FinalProject/spellcheck_talent.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def invited_spelling(df: pd.DataFrame):
+    """
+    
+    :param df: csv dataframe generated from the extraction stage of the ETL pipeline
+    :return: dataframe with correct spelling
+    """
+    for item in df.invited_by_name:
+        if item == "Fifi Eton":
+            item = "Fifi Etton"
+        if item == "Belbrook":
+            item = "Bellbrook"
+    return df
