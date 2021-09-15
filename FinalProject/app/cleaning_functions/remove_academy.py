03c06c1a3a1c6a0d437356b18b935c51ff5c59e3
JDallimore <jdallimore@spartaglobal.com>
2021-09-15 13:57:27 +0100

    Fixed typo

--- FinalProject/app/cleaning_functions/remove_academy.py
+++ FinalProject/app/cleaning_functions/remove_academy.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 def clean_location(df: pd.DataFrame):
-    df["Location"] = df["Location"].map(lambda x: x.replace(" Academy", ""))
+    df["location"] = df["location"].map(lambda x: x.replace(" Academy", ""))
     return df
