422f09056bd464360a3a0196f74714a100b27a04
Vrinda Gera <88028643+CodingUnicorn15@users.noreply.github.com>
2021-09-15 14:41:25 +0100

    Update spellcheck_academy.py
    Changed code

--- FinalProject/spellcheck_academy.py
+++ FinalProject/spellcheck_academy.py
@@ -1,12 +1,10 @@
 import pandas as pd
 
-def trainer_spelling_academy(df: pd.DataFrame):
+def spelling_clean_academy(df: pd.DataFrame):
     """
 
      :param df: csv dataframe generated from the extraction stage of the ETL pipeline
      :return: dataframe with correct spelling
      """
-    for item in df.trainer:
-        if item == "Ely Kely":
-            item = "Elly Kelly"
+    df.trainer.replace("Ely Kely", "Elly Kelly", inplace=True)
     return df
