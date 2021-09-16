49f96f037f73587eb14f6b72f06940a3f52cc017
Conjr123 <Conjr123>
2021-09-16 11:34:09 +0100

    edited code to run off of local files, code now return 3 more dataframes for sequel integration

---
+++ FinalProject/app/df_id_append.py
@@ -0,0 +1,17 @@
+import pandas as pd
+def id_replace_call(json_skill_tables, talent_json_merge):
+    strength_junc = id_replace(json_skill_tables[3], talent_json_merge)
+    weakness_junc = id_replace(json_skill_tables[4], talent_json_merge)
+    tech_self_score = id_replace_skill(json_skill_tables[5], talent_json_merge)
+    return(strength_junc, weakness_junc, tech_self_score)
+
+def id_replace(skill_df, large_df):
+
+    skill_df = skill_df.set_index('internal_id').join(large_df.set_index('internal_id'))
+    skilL_df = skill_df[['candidate_id', 'skill_id' ]]
+    return skilL_df
+
+def id_replace_skill(skill_df, large_df):
+    skill_df = skill_df.set_index('internal_id').join(large_df.set_index('internal_id'))
+    skilL_df = skill_df[['candidate_id', 'skill_id', 'score']]
+    return skilL_df
--- FinalProject/app/save_cleaned_files.py
+++ FinalProject/app/save_cleaned_files.py
@@ -1,15 +1,35 @@
 from Data23_Final_Project.FinalProject.app.cleaning_functions.clean_all_files import *
 from Data23_Final_Project.FinalProject.app.extract_functions.extract_all import *
 from pprint import pprint
-
+import vrindas_join_functions
+import df_id_append
+import pandas as pd
+import json
 
 # extract all files
-extracted = extract_all()
+# extracted = extract_all()
+with open('data.json') as f:
+    extracted1 = json.loads(f.read())
+
+extracted0 = pd.read_csv('academy.csv')
+extracted2 = pd.read_csv('txt.csv')
+extracted3 = pd.read_csv('talent.csv')
+extracted = [extracted0, extracted1, extracted2, extracted3 ]
+
+
 
 # clean all files
 academy_csv, talent_json, talent_txt, talent_csv, json_skill_tables = clean_all_files(extracted)
 
-academy_csv.to_csv('transformed_academy_csv.csv')
-talent_json.to_csv('transformed_talent_json.csv')
-talent_txt.to_csv('transformed_talent_txt.csv')
-talent_csv.to_csv('transformed_talent_csv.csv')
+#
+# academy_csv.to_csv('transformed_academy_csv.csv')
+# talent_json.to_csv('transformed_talent_json.csv')
+# talent_txt.to_csv('transformed_talent_txt.csv')
+# talent_csv.to_csv('transformed_talent_csv.csv')
+
+
+talent_txt_merge = vrindas_join_functions.talent_csv_txt(talent_csv, talent_txt)
+talent_json_merge = vrindas_join_functions.talent_csv_json(talent_csv, talent_json)
+talent_json_merge.to_csv('transformed_academy_csv.csv')
+
+strength_junc, weakness_junc, tech_self_score = df_id_append.id_replace_call(json_skill_tables, talent_json_merge)
