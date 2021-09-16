a225ea797ba9f8c71d0bce5abf957a65be53f85d
Conjr123 <Conjr123>
2021-09-16 11:55:28 +0100

    edited code to run off of local files, code now return 3 more dataframes for sequel integration

---
+++ FinalProject/app/extract_functions/main2.py
@@ -0,0 +1,11 @@
+import json
+from extract_all import *
+extracted = extract_all()
+
+extracted[0].to_csv('academy.csv')
+
+extracted[2].to_csv('txt.csv')
+
+extracted[3].to_csv('talent.csv')
+with open('../data.json', 'w', newline='') as fp:
+    json.dump(extracted[1], fp, sort_keys=True, indent=4)
