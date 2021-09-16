32761fa107903585fbef9a46da3b99a1bcccbd82
Vrinda Gera <88028643+CodingUnicorn15@users.noreply.github.com>
2021-09-16 22:22:04 +0100

    Update vrindas_join_functions.py
    Adding comments because I forgot when I made this

--- FinalProject/app/transform/vrindas_join_functions.py
+++ FinalProject/app/transform/vrindas_join_functions.py
@@ -1,10 +1,24 @@
 
 import pandas as pd
 
+# functiona to merge files
+
 def talent_csv_txt(df: pd.DataFrame, txt: pd.DataFrame):
+    """
+    
+    :param df: csv dataframe generated from the extraction stage of the ETL pipeline
+    :param txt: txt dataframe generated from the extraction stage of the ETL pipeline
+    :return: df and txt dataframes merged on df
+    """
     talent_txt_merge = pd.merge(df, txt, on=['candidate_name', 'applicant_day_date'], how='outer', indicator=True)
     return talent_txt_merge
 
 def talent_csv_json(df: pd.DataFrame, json: pd.DataFrame):
+    """
+
+    :param df: csv dataframe generated from the extraction stage of the ETL pipeline
+    :param json: json dataframe generated from the extraction stage of the ETL pipeline
+    :return: df and json dataframes merged on df
+    """
     talent_json_merge = pd.merge(df, json, on="candidate_name", how='outer', indicator=True)
     return talent_json_merge
